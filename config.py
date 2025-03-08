eaa4cac7236a0e43f3871b78bb3ec23053dddcc2
Jag-kr <jagjeetk@iitbhilai.ac.in>
2025-03-08 19:20:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 import logging
 
-# Twilio Configuration
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
-
 # Application Configuration
 CHUNK_SIZE = 1000
 MAX_WHATSAPP_MESSAGE_LENGTH = 1600
