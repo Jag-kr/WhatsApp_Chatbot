e380fd6502e5bdd417c8a0b2f5dbb5c9359a4e42
Jag-kr <jagjeetk@iitbhilai.ac.in>
2025-03-09 08:19:33 +0530

    Url trim issue

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 
 # Application Configuration
 CHUNK_SIZE = 1000
-MAX_WHATSAPP_MESSAGE_LENGTH = 1550
+MAX_WHATSAPP_MESSAGE_LENGTH = 1580
 DOCUMENT_PATH = "attached_assets/FindMySchool Data.txt"
 
 # Logging Configuration
